303f169503a5cdc903ec6434178453cc00d56868
Ngoc Ngo Dinh (ngocnd0242) <ngo.dinh.ngoc@sun-asterisk.com>
2020-04-11 15:17:08 +0700

    Initial source code

---
+++ src/main/java/com/markdown/mailable/SpringbootMarkdownMailableApplication.java
@@ -0,0 +1,13 @@
+package com.markdown.mailable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMarkdownMailableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMarkdownMailableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markdown/mailable/SpringbootMarkdownMailableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markdown.mailable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMarkdownMailableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
